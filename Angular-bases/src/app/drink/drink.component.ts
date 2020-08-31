6b8c3756f44668ababd745ff104eaf0fe5e7894c
maciel <macielgaleano.jh@gmail.com>
2020-08-31 00:44:59 -0300

    Drink component and the directives are finished

--- Angular-bases/src/app/drink/drink.component.ts
+++ Angular-bases/src/app/drink/drink.component.ts
@@ -10,18 +10,32 @@ export class DrinkComponent implements OnInit {
 
   public title: String = 'Component of dirnk';
   public drinks: Array <Drink>;
+  public names: String[] | null;
+  public branch: string;
 
   constructor() {
+    this.names = new Array();
+    this.branch = 'Coca Cola';
     this.drinks = [
       new Drink('Coke 3lt', 105, 'Coca Cola', 10),
       new Drink('Fanta 3lt', 102, 'Coca Cola', 4),
       new Drink('Fanta pomelo 3lt', 102, 'Coca Cola', 3),
-      new Drink('Sprite', 104, 'Coca Cola', 1)
+      new Drink('Sprite 3lt', 104, 'Coca Cola', 1),
+      new Drink('Fanta uva 3lt', 104, 'Coca Cola', 0)
     ]
    }
 
   ngOnInit(){
     console.log(this.drinks);
+    this.Get_names();
+    console.log(this.names);
   }
+
+  Get_names(){
+    this.drinks.forEach((drink, index) => {
+      this.names.push(drink.name);
+    });
+  }
+
   
 }
