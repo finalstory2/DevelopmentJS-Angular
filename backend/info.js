b2ae11f4777e26fe04e76390b3e828437fc22458
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-05 04:53:57 -0300

    Working with the files of backend, the dependence are mentioned in info.js

---
+++ backend/info.js
@@ -0,0 +1,14 @@
+//Dependences installed
+
+var dependencies = {
+    "body-parser": "^1.19.0", //For parse package
+    "connect-multiparty": "^2.2.0", //For use files in the backend
+    "express": "^4.17.1", //For function backend 
+    "mongoose": "^5.10.3", //For use mongo DB
+    "nodemon": "^2.0.4" //For see the changes on the server
+}
+
+var package_json_scripts = {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon index.js"
+}
